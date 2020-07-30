98aafb642e3293dc0ee9dc28767c194a4bfb6375
特能行 <59129831+Tenengxing@users.noreply.github.com>
2020-07-30 15:50:34 +0800

    支持批量制作，支持64G制作

--- TeslaCamMusicMaker.sh
+++ TeslaCamMusicMaker.sh
@@ -2,12 +2,22 @@
 
 echo "Make Tesla Camera USB"
 
+say "发现`diskutil list | grep external | wc -l`个外置存储，开始制作特斯拉行车记录仪"
+
 # diskutil info `diskutil list | grep "external" | awk '{print $1}'`
 
-diskutil partitiondisk `diskutil list | grep "external" | awk '{print $1}'` MBR FAT32 TESLA 25g FAT32 MUSIC 0
+for dsk in `diskutil list | grep external | awk '{print $1}'`
+do
+  d_size=`diskutil info $dsk | grep "Disk Size" | awk '{print $3}'`
+  if [[ $d_size -lt 33 ]]; then
+    o_size='26g'
+  else
+    o_size='55g'
+  fi
+  diskutil partitiondisk $dsk MBR exFAT TESLA $o_size FAT32 MUSIC 0
+  mkdir -p /Volumes/TESLA/TeslaCam/ /Volumes/TESLA/TeslaTrackMode/
+  diskutil eject $dsk
+done
 
-mkdir -p /Volumes/TESLA/TeslaCam/ /Volumes/TESLA/TeslaTrackMode/
-
-diskutil eject `diskutil list | grep "external" | awk '{print $1}'`
-
+say "制作完成"
 echo "制作完成"
